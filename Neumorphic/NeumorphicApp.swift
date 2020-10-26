9604acdf157e1cff99075cdde8cd8f8f535e4261
KenFai <ngkenfai@gmail.com>
2020-10-26 15:23:51 +0800

    Initial Commit

---
+++ Neumorphic/NeumorphicApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeumorphicApp.swift
+//  Neumorphic
+//
+//  Created by Ginger on 26/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct NeumorphicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
